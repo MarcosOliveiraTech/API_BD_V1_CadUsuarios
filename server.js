3b1e8a14fd21def84fa3f81d6a318be107ac1d22
MarcosOliveiraTech <162984820+MarcosOliveiraTech@users.noreply.github.com>
2024-08-24 23:53:03 -0300

    V2 - Projeto BD + Jason
    Nesta segunda versão comentada agora a possibilidade de cadastrar localmente usuários sem email e depois visualizar as informações cadastradas futuramente estarei utilizando um pouco de dados novo db para tornar mais profissional e segura o armazenamento dessas informações sensíveis além disso adicionei códigos que torna possível a resposta seus dados foram enviados recebidos pelo servidor

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ import express from 'express'
 const app  = express ()
 // Importar todas as informações da biblioteca
 
+app.use(express.json())
+// importar informações em Json para teste de usuários
+
+// Teste de Cadastro de usuários
+// Definir uma lista com informações pré-cadastradas para testes de acesso
+const users = []
+
+
+// Salvar usuários na variável USERS, listada à cima.
+app.post('/usuarios', (req,res) =>{
+    // Requisção
+    users.push(req.body)
+    
+    
+    res.status(201).json(req.body) //Responder ao usuário que a atividade foi concluída com sucesso
+})
+
+// Listar na tela usuários cadastrados em lista JASON
 app.get('/usuarios', (req, res) => {
     
-    res.send('servidor no ar')
+    res.json(users)
+    res.status(200).json(users) //Informar que a atividade foi concluída com sucesso
 
 })
 // Exibir usuários cadastrados
