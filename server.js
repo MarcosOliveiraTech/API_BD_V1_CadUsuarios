2df5b6e07e951115e7b994be8ba88d65f81f6901
MarcosOliveiraTech <162984820+MarcosOliveiraTech@users.noreply.github.com>
2024-08-25 23:33:19 -0300

    Último commit de versão 01 - Estrutura completa!
    Concluída a primeira versão da aplicação
    
    Concluo a primeira versão da aplicação, agora é possível cadastrar, visualizar e deletar informações de usuários (nome, idade e email). 
    
    Foram adicionadas duas bibliotecas essenciais:
    1. **MongoDB:** Banco de dados NoSQL compatível com JSON, que facilita o armazenamento e manipulação de dados através do JavaScript.
    2. **Prisma:** Biblioteca que simplifica a comunicação e o gerenciamento de dados no MongoDB, abstraindo a complexidade e otimizando o desempenho.
    
    Com essas ferramentas e estruturas devidamente configuradas e desenvolvidas, a aplicação está pronta para expansão. A próxima versão incluirá uma interface amigável para gerenciar as informações de maneira ainda mais eficiente.

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-import express from 'express'
+import express, { query } from 'express'
 // Instalar recursos da biblioteca neste projeto, ativando seus recursos
+
+// Importar recusos da biblioteca PRISMA para enviar ao BD
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... Escrever as propriedades dos usuários aqui
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
 const app  = express ()
 // Importar todas as informações da biblioteca
 
@@ -7,27 +26,89 @@ app.use(express.json())
 // importar informações em Json para teste de usuários
 
 // Teste de Cadastro de usuários
-// Definir uma lista com informações pré-cadastradas para testes de acesso
-const users = []
-
 
 // Salvar usuários na variável USERS, listada à cima.
-app.post('/usuarios', (req,res) =>{
+app.post('/usuarios', async (req,res) =>{
     // Requisção
-    users.push(req.body)
+
+    // promisse
+    // await: Aguardar resposta do banco de dados
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
     
     
     res.status(201).json(req.body) //Responder ao usuário que a atividade foi concluída com sucesso
 })
 
 // Listar na tela usuários cadastrados em lista JASON
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
     
-    res.json(users)
-    res.status(200).json(users) //Informar que a atividade foi concluída com sucesso
+    let users = []
+
+    if(req.query.name) {
+
+        //Informar que a atividade foi concluída com sucesso
+        users = await prisma.user.findMany({
+            where: {
+                name: {
+                    contains: req.query.name, 
+                    mode: 'insensitive'
+                }
+            
+            },
+        })
+
+    } else {
+        users = await prisma.user.findMany()
+    }
+    
+    
+    res.status(200).json(users); 
 
 })
 // Exibir usuários cadastrados
+
+
+// Editar um usuário
+app.put('/usuarios/:id', async (req,res) =>{
+    // Requisção
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    
+    
+    res.status(201).json(req.body) //Responder ao usuário que a atividade foi concluída com sucesso
+})
+
+// Deletar um usuário
+app.delete('/usuarios/:id', async (req,res) =>{
+    
+    // Deletar nome em BD a partir do Login
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+
+        
+    })
+    
+    
+    res.status(203).json({message:'usuario deletado'}) //Responder ao usuário que a atividade foi concluída com sucesso
+})
 
 
 app.listen(3000)
